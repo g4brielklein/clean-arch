61677adcbaba5b56ce3dbe2e033ff73d834146dc
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-09-09 21:21:29 -0300

    add unit tests

---
+++ clean-code/tests/validateCpf.test.ts
@@ -0,0 +1,22 @@
+import { validateCpf } from "../src/validateCpf";
+
+// Parameterized tests
+test.each([
+    "97456321558",
+    "714287938-60",
+    "974.563.215-58",
+    "714.287.938-60"
+])('Should validate CPF %s', (cpf: string) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(true);
+})
+
+test.each([
+    "",
+    null,
+    undefined,
+    "111.111.111-11"
+])('Should not validate CPF %s', (cpf: any) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(false);
+})
---
+++ clean-code/tests/validatePassword.test.ts
@@ -0,0 +1,28 @@
+import { validatePassword } from "../src/validatePassword";
+
+test.each([
+    "aBc123az",
+    "SADISAU11a",
+    "23298393aZ",
+    "a12345678Z",
+])('Should validate password %s', (password: string) => {
+    // Should have uppercase characters, lowercase and numbers
+    // Sould have at least 8 characters
+
+    const isValid = validatePassword(password);
+    expect(isValid).toBe(true)
+})
+
+test.each([
+    "",
+    "asD123",
+    "12345678",
+    "asdfghjkl",
+    "ASDFGHJKL",
+])('Should not validate password %s', (password: string) => {
+    // Should have uppercase characters, lowercase and numbers
+    // Sould have at least 8 characters
+
+    const isValid = validatePassword(password);
+    expect(isValid).toBe(false)
+})
