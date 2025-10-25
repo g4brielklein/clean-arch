cdbf493c9ed68eb71d82137c7a091ce51b328c69
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-10-24 21:47:38 -0300

    improve ORM tests

--- tests/integration/ORM.test.ts
+++ tests/integration/ORM.test.ts
@@ -3,20 +3,46 @@ import DatabaseConnection, { PgPromiseAdapter } from "../../src/infra/database/D
 import Registry from "../../src/infra/di/Registry";
 import ORM, { AccountModel } from "../../src/infra/orm/ORM";
 
+let orm: ORM;
 let databaseConnection: DatabaseConnection;
 
 test("Should persist on db using an ORM", async () => {
     databaseConnection = new PgPromiseAdapter();
     Registry.getInstance().provide("databaseConnection", databaseConnection);
-    const orm = new ORM();
+    orm = new ORM();
     Registry.getInstance().provide("orm", orm);
-    const accountId = UUID.create().getValue();
-    const accountModel = new AccountModel(accountId, "Michael Scott", "mscott@dundermifflin.com", '97456321558', "DunderMifflin1", '', true, false);
+
+    const inputAccount = {
+        accountId: UUID.create().getValue(),
+        name: "Michael Scott",
+        email: "mscott@dundermifflin.com",
+        cpf: "97456321558",
+        password: "DunderMifflin1",
+        carPlate: "",
+        isPassenger: true,
+        isDriver: false,
+    }
+    const accountModel = new AccountModel(
+        inputAccount.accountId, 
+        inputAccount.name, 
+        inputAccount.email, 
+        inputAccount.cpf, 
+        inputAccount.password, 
+        inputAccount.carPlate, 
+        inputAccount.isPassenger, 
+        inputAccount.isDriver
+    );
     await orm.save(accountModel);
-    const account = await orm.get(AccountModel, 'account_id', accountId);
-    expect(account.accountId).toBe(accountId);
-    expect(account.name).toBe("Michael Scott");
-    expect(account.email).toBe("mscott@dundermifflin.com")
+
+    const account = await orm.get(AccountModel, 'account_id', inputAccount.accountId);
+    expect(account.accountId).toBe(inputAccount.accountId);
+    expect(account.name).toBe(inputAccount.name);
+    expect(account.email).toBe(inputAccount.email);
+    expect(account.cpf).toBe(inputAccount.cpf);
+    expect(account.password).toBe(inputAccount.password);
+    expect(account.carPlate).toBe(inputAccount.carPlate);
+    expect(account.isPassenger).toBe(inputAccount.isPassenger);
+    expect(account.isDriver).toBe(inputAccount.isDriver);
 })
 
 afterEach(async () => {
