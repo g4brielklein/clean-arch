df1438a7592635d732e5015fc88892bb1a26312b
Gabriel Klein <gabrielkleinribas@gmail.com>
2025-11-22 11:35:10 -0300

    add payment processor chain of responsability

---
+++ src/infra/fallback/PaymentProcessor.ts
@@ -0,0 +1,48 @@
+// Chain of Responsability
+
+import { CieloPaymentGateway, PJBankPaymentGateway } from "../gateway/PaymentGateway";
+
+export default interface PaymentProcessor {
+    next?: PaymentProcessor,
+    processPayment (input: any): Promise<any>,
+}
+
+export class CieloPaymentProcessor implements PaymentProcessor {
+    constructor(readonly next?: PaymentProcessor) {}
+
+    async processPayment(input: any): Promise<any> {
+        try {
+            const gateway = new CieloPaymentGateway();
+            const output = await gateway.processTransaction(input);
+            return output;
+        } catch(_: any) {
+            if (!this.next) throw new Error('No processors available');
+            return this.next.processPayment(input);
+        }
+    }
+}
+
+export class PJBankPaymentProcessor implements PaymentProcessor {
+    constructor(readonly next?: PaymentProcessor) {}
+
+    async processPayment(input: any): Promise<any> {
+        try {
+            const gateway = new PJBankPaymentGateway();
+            const output = await gateway.processTransaction(input);
+            return output;
+        } catch(_: any) {
+            if (!this.next) throw new Error('No processors available');
+            return this.next.processPayment(input);
+        }
+    }
+}
+
+export class PaymentProcessorFactory {
+    static create () {
+        const cieloProcessor = new CieloPaymentProcessor();
+        const pjBankProcessor = new PJBankPaymentProcessor(cieloProcessor);
+
+        // default processor
+        return pjBankProcessor;
+    }
+}
